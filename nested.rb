f78ca7b8bac8c136216433e4d90f5c31e1f2f94f
Michael Pope <popemray1986@gmail.com>
2017-10-22 21:07:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihio_matsumot] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash[:yukihio_matsumot] = {known_for: => "Ruby", languages: => ["LISP", "C"]}
 end
 
 def changing_alan
