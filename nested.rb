df6c702a80a2b6e18c44e6527579d93d5ba598ce
Michael Pope <popemray1986@gmail.com>
2017-10-22 20:37:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,8 +61,8 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
+programmer_hash[:yukihio_matsumot => {}]
+# :yukihio_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
